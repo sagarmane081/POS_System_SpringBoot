584aa7000b3c5f08b521baf72cbebca1744b23fc
optimisticprime026 <sagarmane0026@gmail.com>
2026-03-04 02:14:23 +0900

    Initial commit

---
+++ src/main/java/com/sagar/SagarPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagarPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagarPosSystemApplication.class, args);
+	}
+
+}
